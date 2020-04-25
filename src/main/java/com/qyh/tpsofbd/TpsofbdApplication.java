4cdd7806fa007f12f50476a244db9abaab4962c8
WoSiGuWoZai-QYH <13007619713@163.com>
2020-04-25 22:45:01 +0800

    first commit

---
+++ src/main/java/com/qyh/tpsofbd/TpsofbdApplication.java
@@ -0,0 +1,13 @@
+package com.qyh.tpsofbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpsofbdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpsofbdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qyh/tpsofbd/TpsofbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qyh.tpsofbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpsofbdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
