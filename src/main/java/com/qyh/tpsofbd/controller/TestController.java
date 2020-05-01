bbd517b7340f5f6914363aa30384d94d20cd5190
WoSiGuWoZai-QYH <13007619713@163.com>
2020-05-01 21:28:29 +0800

    配置logback日志打印

--- src/main/java/com/qyh/tpsofbd/controller/TestController.java
+++ src/main/java/com/qyh/tpsofbd/controller/TestController.java
@@ -1,7 +1,41 @@
 package com.qyh.tpsofbd.controller;
 
+import com.qyh.tpsofbd.config.ServerSettings;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/test")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class TestController {
-    String address="sd";
-    int age=1;
-    String tel="ssds";
+
+    private Map<String,Object> params=new HashMap<>();
+
+    private final ServerSettings serverSettings;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+
+    @GetMapping("/propertie")
+    public Object testProperties(){
+
+        //params.clear();
+        return serverSettings;
+    }
+
+    @GetMapping("/log")
+    public Object testLog(){
+
+        LOGGER.debug("this is debug level");
+        LOGGER.info("this is info level");
+        LOGGER.warn("this is warn level");
+        LOGGER.error("this is error level");
+        return serverSettings;
+    }
 }
