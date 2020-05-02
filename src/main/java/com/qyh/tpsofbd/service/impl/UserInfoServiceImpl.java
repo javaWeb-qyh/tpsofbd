fe0e82d0f484d05c048947e5bcaf7c0f7d44d077
WoSiGuWoZai-QYH <13007619713@163.com>
2020-05-02 18:32:44 +0800

    验证mybatis-spring包的作用：目前没发现其作用，不影响程序运行，暂将其移除

--- src/main/java/com/qyh/tpsofbd/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/qyh/tpsofbd/service/impl/UserInfoServiceImpl.java
@@ -8,8 +8,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED)
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class UserInfoServiceImpl implements UserInfoService {
 
